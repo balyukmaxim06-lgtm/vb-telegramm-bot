1fca0326b5096f605b8628078e5fcfc82d546a51
balyukmaxim06-lgtm <balyuk.maxim06@gmail.com>
2026-09-05 23:47:50 +0300

    Update test.py

--- test.py
+++ test.py
@@ -44,7 +44,7 @@ def health():
 # ============================================================
 
 # ⚠️ ВСТАВЬ СВОЙ НОВЫЙ ТОКЕН (СБРОШЕННЫЙ ЧЕРЕЗ @BotFather)
-TOKEN = "ТВОЙ_НОВЫЙ_ТОКЕН_ОТ_BOTFATHER"
+TOKEN = "8818834067:AAGZFrrlXShenGh4Pb8NllTLePxjbh9RRdw"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
